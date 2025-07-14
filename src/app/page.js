b06531d30bfc3bc8104e8d902d25831b071bd367
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-14 22:10:02 +0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+import s from "./page.module.scss";
+
+export default function Home() {
+  return <main className={s.home}></main>;
+}
---
+++ src/redux/slices/globalSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const globalSlice = createSlice({
+  initialState,
+  name: "globalSlice",
+  reducers: {},
+});
+
+export default globalSlice.reducer;
+export const {} = globalSlice.actions;
