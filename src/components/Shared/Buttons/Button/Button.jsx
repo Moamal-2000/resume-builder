23dfc908b715901e38b01cb96f1fe6e1fa6d7549
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-15 02:19:34 +0300

    init: reusable Button component

---
+++ src/components/Shared/Buttons/Button/Button.jsx
@@ -0,0 +1,11 @@
+import s from "./Button.module.scss";
+
+const Button = () => {
+  return (
+    <button type="button" className={s.buttons}>
+      Button
+    </button>
+  );
+};
+
+export default Button;
