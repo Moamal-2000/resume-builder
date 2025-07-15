cf2de9bfbacb690deaa10bd73e865b2f69d76cc9
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-16 00:55:59 +0300

    feat(Button): support rendering as <a> tag when 'href' prop is present

--- src/components/Shared/Buttons/Button/Button.jsx
+++ src/components/Shared/Buttons/Button/Button.jsx
@@ -1,6 +1,29 @@
+import Link from "next/link";
 import s from "./Button.module.scss";
 
-const Button = ({ children, fillWidth, paddingInline = "20px", onClick }) => {
+const Button = ({
+  children,
+  fillWidth,
+  paddingInline = "20px",
+  onClick,
+  href,
+}) => {
+  if (href)
+    return (
+      <Link
+        href={href}
+        type="button"
+        className={s.button}
+        onClick={onClick}
+        style={{
+          width: fillWidth ? "100%" : "fit-content",
+          paddingInline,
+        }}
+      >
+        {children}
+      </Link>
+    );
+
   return (
     <button
       type="button"
