222759442a60f770d21689831155b90ff172cb6e
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-15 15:00:01 +0300

    feat(Education): build the structure & add basic styles

--- src/components/HomePage/MyResume/Education/Education.jsx
+++ src/components/HomePage/MyResume/Education/Education.jsx
@@ -1,7 +1,17 @@
 import s from "./Education.module.scss";
 
 const Education = () => {
-  return <section className={s.education}></section>;
+  return (
+    <section className={s.education}>
+      <h2>Education</h2>
+
+      <div className={s.educationContent}>
+        <p className={s.degree}>B.Sc in Computer Science</p>
+        <p className={s.university}>National Open University of Nigeria</p>
+        <div className={s.graduateDate}>2015 - 2019</div>
+      </div>
+    </section>
+  );
 };
 
 export default Education;
