4da7bd936ac2d11ee3cde12b27ce4bacb15d7fd7
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-15 19:51:27 +0300

    update(Experiences): render content by mapping over MOCK_EXPERIENCE data

--- src/components/HomePage/MyResume/Experiences/Experiences.jsx
+++ src/components/HomePage/MyResume/Experiences/Experiences.jsx
@@ -1,7 +1,24 @@
+import { MOCK_EXPERIENCE } from "@/data/staticData";
 import s from "./Experiences.module.scss";
+import JobExperience from "./JobExperience/JobExperience";
 
 const Experiences = () => {
-  return <section className={s.experiences}></section>;
+  return (
+    <section className={s.experiences}>
+      {MOCK_EXPERIENCE.map(
+        ({ title, company, startDate, endDate, description, id }) => (
+          <JobExperience
+            key={id}
+            title={title}
+            company={company}
+            startDate={startDate}
+            endDate={endDate}
+            description={description}
+          />
+        )
+      )}
+    </section>
+  );
 };
 
 export default Experiences;
