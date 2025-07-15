9776f20236ca53c5d6c6c76c2d17a605afc86264
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-15 03:07:53 +0300

    style(Header): build header structure with its styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../Shared/Buttons/Button/Button";
 import s from "./Header.module.scss";
 
 const Header = () => {
-  return <header className={s.header}></header>;
+  return (
+    <header className={s.header}>
+      <Link href="/" className={s.logo} aria-label="Go to Homepage">
+        <Image
+          src="/assets/images/logo.png"
+          alt="CB ceVBuilder logo"
+          width={180}
+          height={56}
+          priority
+        />
+      </Link>
+
+      <Button>Get Started</Button>
+    </header>
+  );
 };
 
 export default Header;
