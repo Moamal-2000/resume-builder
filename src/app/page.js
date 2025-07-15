c9dcd587260030b82a0215fff0c77caee0737320
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-15 03:20:05 +0300

    style(HeroSection): build hero section structure with its styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import HeroSection from "@/components/HomePage/HeroSection/HeroSection";
 import s from "./page.module.scss";
 
 export default function Home() {
-  return <main className={s.home}></main>;
+  return (
+    <main className={s.home}>
+      <HeroSection />
+    </main>
+  );
 }
--- src/components/HomePage/HeroSection/HeroSection.jsx
+++ src/components/HomePage/HeroSection/HeroSection.jsx
@@ -1,7 +1,24 @@
+import Button from "@/components/Shared/Buttons/Button/Button";
 import s from "./HeroSection.module.scss";
 
 const HeroSection = () => {
-  return <section className={s.heroSection}>HeroSection</section>;
+  return (
+    <section className={s.heroSection}>
+      <h1>
+        Create
+        <br />a career in tech.
+      </h1>
+
+      <p>
+        The template includes carefully structured sections for personal
+        information, summary or objective, work experience, education, skills,
+        projects, certifications, and more. You can easily customize these
+        sections to fit your unique background.
+      </p>
+
+      <Button>Create Resume for free</Button>
+    </section>
+  );
 };
 
 export default HeroSection;
