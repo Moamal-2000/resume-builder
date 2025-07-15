5b0a74b856c2a074a6dfdcb73ff8cc3222ed79ae
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-15 15:21:33 +0300

    init: TechSkills component

--- src/components/HomePage/MyResume/MyResume.jsx
+++ src/components/HomePage/MyResume/MyResume.jsx
@@ -1,12 +1,14 @@
 import Education from "./Education/Education";
 import s from "./MyResume.module.scss";
 import ProfileHeader from "./ProfileHeader/ProfileHeader";
+import TechSkills from "./TechSkills/TechSkills";
 
 const MyResume = () => {
   return (
     <section className={s.myResume}>
       <ProfileHeader />
       <Education />
+      <TechSkills />
     </section>
   );
 };
---
+++ src/components/HomePage/MyResume/TechSkills/TechSkills.jsx
@@ -0,0 +1,7 @@
+import s from "./TechSkills.module.scss";
+
+const TechSkills = () => {
+  return <section className={s.techSkills}></section>;
+};
+
+export default TechSkills;
