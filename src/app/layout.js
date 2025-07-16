2759b56eeb6b38e53e00c556430d4c5db7559f64
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-17 01:27:18 +0300

    update(metadata): fix website title typo

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "../styles/globals.scss";
 import RootProviders from "./RootProviders";
 
 export const metadata = {
-  title: "Builder Resume | Home",
+  title: "Resume Builder | Home",
   description:
     "Build a professional resume effortlessly with our intuitive online CV builder. Customize sections, preview in real time, and download your resume in high quality — no registration required.",
 };
