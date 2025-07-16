b23cf8eb1d2a7b2a95abc4bffe00aabd01cc7701
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-16 16:41:42 +0300

    style(BuilderLayout): use flex layout with spacing and switch container to div

--- src/app/(pages)/builder/layout.js
+++ src/app/(pages)/builder/layout.js
@@ -1,12 +1,13 @@
 import Sidebar from "@/components/BuilderPage/Sidebar/Sidebar/Sidebar";
+import s from "./Layout.module.scss";
 
 const BuilderLayout = ({ children }) => {
   return (
-    <main>
+    <div className={s.builderLayout}>
       <Sidebar />
       {children}
       <h2>Live Resume Component</h2>
-    </main>
+    </div>
   );
 };
 
