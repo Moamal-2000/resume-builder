34e114447e3cf009c82b97b0d1b2f1d2ecaf8034
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-16 15:43:28 +0300

    feat(Sidebar): dynamically apply styles to the active page link using current route

--- src/components/BuilderPage/Sidebar/Sidebar/Sidebar.jsx
+++ src/components/BuilderPage/Sidebar/Sidebar/Sidebar.jsx
@@ -1,9 +1,14 @@
+"use client";
+
 import SvgIcon from "@/components/Shared/SvgIcon";
 import { BUILDER_SIDEBAR_PAGES } from "@/data/builderPages";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import s from "./Sidebar.module.scss";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className={s.sidebar}>
       <button type="button">
@@ -12,14 +17,18 @@ const Sidebar = () => {
 
       <nav className={s.pagesNav}>
         <ul>
-          {BUILDER_SIDEBAR_PAGES.map(({ title, link, iconName, id }) => (
-            <li key={id}>
-              <Link href={link}>
-                <SvgIcon name={iconName} />
-                {title}
-              </Link>
-            </li>
-          ))}
+          {BUILDER_SIDEBAR_PAGES.map(({ title, link, iconName, id }) => {
+            const activeClass = link === pathname ? s.active : "";
+
+            return (
+              <li key={id}>
+                <Link href={link} className={activeClass}>
+                  <SvgIcon name={iconName} />
+                  {title}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </aside>
