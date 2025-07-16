b73318bef0831c678f8f50767c4f44a136d2801e
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-17 01:49:21 +0300

    feat(Input): support basic props and optional label rendering

--- src/components/Shared/Inputs/Input/Input.jsx
+++ src/components/Shared/Inputs/Input/Input.jsx
@@ -1,7 +1,27 @@
 import s from "./Input.module.scss";
 
-const Input = () => {
-  return <input type="text" className={s.input} />;
+const Input = ({
+  label,
+  type = "text",
+  name,
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return (
+    <div className={s.input}>
+      {label && <label htmlFor={name}>{label}</label>}
+
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 };
 
 export default Input;
