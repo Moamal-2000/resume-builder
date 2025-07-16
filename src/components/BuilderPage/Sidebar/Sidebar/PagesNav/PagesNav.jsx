63c120cc55729c9524971766eae307266ae5a92d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-16 21:56:53 +0300

    feat(PagesNav): sync icon movement and text visibility with sidebar toggle

--- src/components/BuilderPage/Sidebar/Sidebar/PagesNav/PagesNav.jsx
+++ src/components/BuilderPage/Sidebar/Sidebar/PagesNav/PagesNav.jsx
@@ -4,13 +4,16 @@ import SvgIcon from "@/components/Shared/SvgIcon";
 import { BUILDER_SIDEBAR_PAGES } from "@/data/builderPages";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useSelector } from "react-redux";
 import s from "./PagesNav.module.scss";
 
 const PagesNav = () => {
   const pathname = usePathname();
+  const { isAsideOpen } = useSelector((s) => s.global);
+  const closeMenuClass = isAsideOpen ? "" : s.closeMenu;
 
   return (
-    <nav className={s.pagesNav}>
+    <nav className={`${s.pagesNav} ${closeMenuClass}`}>
       <ul>
         {BUILDER_SIDEBAR_PAGES.map(({ title, link, iconName, id }) => {
           const activeClass = link === pathname ? s.active : "";
@@ -19,7 +22,7 @@ const PagesNav = () => {
             <li key={id}>
               <Link href={link} className={activeClass}>
                 <SvgIcon name={iconName} />
-                {title}
+                <span className={s.title}>{title}</span>
               </Link>
             </li>
           );
