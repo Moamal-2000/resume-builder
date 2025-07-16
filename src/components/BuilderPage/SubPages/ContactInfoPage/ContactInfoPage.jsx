b79404441d2f3b80689bca44ee1f8f8b1347548a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-17 01:38:32 +0300

    init: ContactInfoPage component

---
+++ src/components/BuilderPage/SubPages/ContactInfoPage/ContactInfoPage.jsx
@@ -0,0 +1,7 @@
+import s from "./ContactInfoPage.module.scss";
+
+const ContactInfoPage = () => {
+  return <main className={s.contactInfoPage}>ContactInfoPage</main>;
+};
+
+export default ContactInfoPage;
