2b189d15f6c6b6349c3404f4f39808f8683e165a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-17 21:33:22 +0300

    style(PersonalInfoForm): wrap all input fields in a parent div and apply bottom margin

--- src/components/BuilderPage/PersonalInfo/PersonalInfoForm/PersonalInfoForm.jsx
+++ src/components/BuilderPage/PersonalInfo/PersonalInfoForm/PersonalInfoForm.jsx
@@ -12,15 +12,17 @@ const PersonalInfoForm = () => {
 
   return (
     <form className={s.personalInfoForm} onSubmit={handleSubmit}>
-      {PERSONAL_INFO_INPUTS.map(({ label, name, placeholder, type, id }) => (
-        <Input
-          key={id}
-          label={label}
-          name={name}
-          placeholder={placeholder}
-          type={type}
-        />
-      ))}
+      <div className={s.inputs}>
+        {PERSONAL_INFO_INPUTS.map(({ label, name, placeholder, type, id }) => (
+          <Input
+            key={id}
+            label={label}
+            name={name}
+            placeholder={placeholder}
+            type={type}
+          />
+        ))}
+      </div>
 
       <Button fillWidth={true} type="submit">
         Next
