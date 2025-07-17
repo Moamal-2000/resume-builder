f56acb114d1570e9600b146037585a4fb1446fb1
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-17 04:16:55 +0300

    refactor(Sidebar): isolate sidebar icon logic into SidebarIcon component

--- src/components/BuilderPage/Sidebar/Sidebar/Sidebar.jsx
+++ src/components/BuilderPage/Sidebar/Sidebar/Sidebar.jsx
@@ -1,56 +1,32 @@
 "use client";
 
 import ProgressBar from "@/components/Shared/ProgressBar/ProgressBar";
-import SvgIcon from "@/components/Shared/SvgIcon";
-import useAnimationStatus from "@/hooks/helper/useAnimationStatus";
-import { updateGlobalState } from "@/redux/slices/globalSlice";
-import { useEffect, useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useRef } from "react";
+import { useSelector } from "react-redux";
 import PagesNav from "./PagesNav/PagesNav";
 import s from "./Sidebar.module.scss";
 import SidebarIcon from "./SidebarIcon/SidebarIcon";
 
 const Sidebar = () => {
-  const dispatch = useDispatch();
   const { isAsideOpen } = useSelector((s) => s.global);
 
-  const sidebarIcon = isAsideOpen ? "menu" : "burgerMenu";
-  const [sidebarIconName, setSidebarIconName] = useState(sidebarIcon);
-
   const sidebarRef = useRef();
-  const closedOnce = useRef(false);
-  const debounceTimeout = useRef(null);
-  const isSidebarAnimating = useAnimationStatus(sidebarRef);
+  const closedOnceRef = useRef(false);
 
   const closeClass = isAsideOpen ? s.open : s.close;
-  const closedOnceClass = closedOnce.current ? s.closedOnce : "";
+  const closedOnceClass = closedOnceRef.current ? s.closedOnce : "";
   const resumeProgress = "10%";
-
-  function handleToggleAside() {
-    if (!closedOnce.current) closedOnce.current = true;
-    if (isSidebarAnimating.current) return;
-
-    dispatch(updateGlobalState({ key: "isAsideOpen", value: !isAsideOpen }));
-  }
-
-  useEffect(() => {
-    clearTimeout(debounceTimeout.current);
-
-    debounceTimeout.current = setTimeout(() => {
-      setSidebarIconName(sidebarIcon);
-    }, 900);
-  }, [isAsideOpen]);
 
   return (
     <aside
       className={`${s.sidebar} ${closeClass} ${closedOnceClass}`}
       ref={sidebarRef}
     >
-      <SidebarIcon />
-      <button type="button" onClick={handleToggleAside} className={closeClass}>
-        <SvgIcon name={sidebarIconName} />
-      </button>
-
+      <SidebarIcon
+        isAsideOpen={isAsideOpen}
+        sidebarRef={sidebarRef}
+        closedOnceRef={closedOnceRef}
+      />
       <PagesNav />
       <ProgressBar progress={resumeProgress} relatedTo="sidebar" />
     </aside>
--- src/components/BuilderPage/Sidebar/Sidebar/SidebarIcon/SidebarIcon.jsx
+++ src/components/BuilderPage/Sidebar/Sidebar/SidebarIcon/SidebarIcon.jsx
@@ -1,9 +1,46 @@
-import s from './SidebarIcon.module.scss'
+"use client";
 
-const SidebarIcon = () => {
+import SvgIcon from "@/components/Shared/SvgIcon";
+import useAnimationStatus from "@/hooks/helper/useAnimationStatus";
+import { updateGlobalState } from "@/redux/slices/globalSlice";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import s from "./SidebarIcon.module.scss";
+
+const SidebarIcon = ({ isAsideOpen, sidebarRef, closedOnceRef }) => {
+  const sidebarIcon = isAsideOpen ? "menu" : "burgerMenu";
+  const [sidebarIconName, setSidebarIconName] = useState(sidebarIcon);
+
+  const dispatch = useDispatch();
+  const debounceTimeout = useRef(null);
+  const closeClass = isAsideOpen ? "" : s.close;
+
+  const isSidebarAnimating = useAnimationStatus(sidebarRef);
+
+  function handleToggleAside() {
+    if (!closedOnceRef.current) closedOnceRef.current = true;
+    if (isSidebarAnimating.current) return;
+
+    dispatch(updateGlobalState({ key: "isAsideOpen", value: !isAsideOpen }));
+  }
+
+  useEffect(() => {
+    clearTimeout(debounceTimeout.current);
+
+    debounceTimeout.current = setTimeout(() => {
+      setSidebarIconName(sidebarIcon);
+    }, 900);
+  }, [isAsideOpen]);
+
   return (
-    <div>SidebarIcon</div>
-  )
-}
+    <button
+      type="button"
+      onClick={handleToggleAside}
+      className={`${s.button} ${closeClass}`}
+    >
+      <SvgIcon name={sidebarIconName} />
+    </button>
+  );
+};
 
-export default SidebarIcon
+export default SidebarIcon;
