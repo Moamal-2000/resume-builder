27ee079b9ae74a3d3cb7e63cf6c728538fcb8f0f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-17 21:26:49 +0300

    feat(PersonalInfoForm): prevent the default behavior of submit event

--- src/components/BuilderPage/PersonalInfo/PersonalInfoForm/PersonalInfoForm.jsx
+++ src/components/BuilderPage/PersonalInfo/PersonalInfoForm/PersonalInfoForm.jsx
@@ -1,11 +1,17 @@
+"use client";
+
 import Button from "@/components/Shared/Buttons/Button/Button";
 import Input from "@/components/Shared/Inputs/Input/Input";
 import { PERSONAL_INFO_INPUTS } from "@/data/builderFormsData";
 import s from "./PersonalInfoForm.module.scss";
 
 const PersonalInfoForm = () => {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <form className={s.personalInfoForm}>
+    <form className={s.personalInfoForm} onSubmit={handleSubmit}>
       {PERSONAL_INFO_INPUTS.map(({ label, name, placeholder, type, id }) => (
         <Input
           key={id}
