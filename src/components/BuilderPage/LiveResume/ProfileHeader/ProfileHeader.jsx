ec7b0c6ce001811bdf1410d5f3cd4b1078aa2c36
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-19 03:02:37 +0300

    fix(ProfileHeader): reorder personal info inputs and enable live editing for profession and email fields

--- src/components/BuilderPage/LiveResume/ProfileHeader/ProfileHeader.jsx
+++ src/components/BuilderPage/LiveResume/ProfileHeader/ProfileHeader.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import SvgIcon from "@/components/Shared/SvgIcon";
-import { MY_SOCIAL_MEDIA } from "@/data/staticData";
 import { getInputValueOrFallback } from "@/functions/helper";
 import useFormsStore from "@/stores/forms.store/forms.store";
 import s from "./ProfileHeader.module.scss";
@@ -9,25 +8,27 @@ import s from "./ProfileHeader.module.scss";
 const ProfileHeader = () => {
   const personalInfoInputs = useFormsStore((s) => s.personalInfoInputs);
 
-  const fullName = getInputValueOrFallback(personalInfoInputs[0]);
+  const [fullName, profession, email] = [
+    getInputValueOrFallback(personalInfoInputs[0]),
+    getInputValueOrFallback(personalInfoInputs[1]),
+    getInputValueOrFallback(personalInfoInputs[2]),
+  ];
 
   return (
     <section className={s.profileHeader}>
       <h2>{fullName}</h2>
+      <h2>{profession}</h2>
 
       <div className={s.socialMedia}>
-        {MY_SOCIAL_MEDIA.map(({ socialName, title, link, iconName, id }) => (
-          <a
-            href={link}
-            className={s.media}
-            key={id}
-            target="_blank"
-            rel="noreferrer noopenner"
-          >
-            <SvgIcon name={iconName} />
-            <span>{title}</span>
-          </a>
-        ))}
+        <a
+          href={email}
+          className={s.media}
+          target="_blank"
+          rel="noreferrer noopenner"
+        >
+          <SvgIcon name="email" />
+          <span>{email}</span>
+        </a>
       </div>
 
       <p>
