925de17743476a1c8b1a2fc94f6f3b47d5398825
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-21 23:52:15 +0300

    refactor(PagesNav): navigation links into FormLink component

--- src/components/BuilderPage/Sidebar/PagesNav/FormLink/FormLink.jsx
+++ src/components/BuilderPage/Sidebar/PagesNav/FormLink/FormLink.jsx
@@ -1,9 +1,40 @@
-import s from './FormLink.module.scss'
+"use client";
 
-const FormLink = () => {
+import SvgIcon from "@/components/Shared/SvgIcon";
+import useFormsStore from "@/stores/forms.store/forms.store";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import s from "./FormLink.module.scss";
+
+const FormLink = ({ linkData, shouldHideTitle }) => {
+  const { title, link, iconName, unlockAfter } = linkData;
+  const formsStore = useFormsStore((s) => s);
+  const pathname = usePathname();
+  const activeClass = link === pathname ? s.active : "";
+
+  const formInputs = Object?.values(
+    formsStore?.[unlockAfter] || formsStore.personalInfoInputs
+  );
+
+  const isFormFilled = formInputs
+    .flat()
+    .every((input) => input.isValidValue || !input.required);
+
+  let disableClass = !isFormFilled ? s.disabled : "";
+  const hideTitleClass = shouldHideTitle ? s.hideTitle : "";
+  if (unlockAfter === "none") disableClass = "";
+
   return (
-    <div>FormLink</div>
-  )
-}
+    <li className={s.listItem}>
+      <Link
+        href={link}
+        className={`${activeClass} ${disableClass} ${hideTitleClass}`}
+      >
+        <SvgIcon name={iconName} />
+        <span className={s.title}>{title}</span>
+      </Link>
+    </li>
+  );
+};
 
-export default FormLink
+export default FormLink;
--- src/components/BuilderPage/Sidebar/PagesNav/PagesNav.jsx
+++ src/components/BuilderPage/Sidebar/PagesNav/PagesNav.jsx
@@ -1,46 +1,24 @@
 "use client";
 
-import SvgIcon from "@/components/Shared/SvgIcon";
 import { BUILDER_SIDEBAR_PAGES } from "@/data/builderPages";
-import useFormsStore from "@/stores/forms.store/forms.store";
 import useGlobalStore from "@/stores/global.store/global.store";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import FormLink from "./FormLink/FormLink";
 import s from "./PagesNav.module.scss";
 
 const PagesNav = () => {
   const isAsideOpen = useGlobalStore((s) => s.isAsideOpen);
-  const formsStore = useFormsStore((s) => s);
-  const pathname = usePathname();
   const closeMenuClass = isAsideOpen ? "" : s.closeMenu;
 
   return (
     <nav className={`${s.pagesNav} ${closeMenuClass}`}>
       <ul>
-        {BUILDER_SIDEBAR_PAGES.map(
-          ({ title, link, iconName, unlockAfter, id }) => {
-            const activeClass = link === pathname ? s.active : "";
-
-            const formInputs = Object?.values(
-              formsStore?.[unlockAfter] || formsStore.personalInfoInputs
-            );
-            const isFormFilled = formInputs
-              .flat()
-              .every((input) => input.isValidValue);
-
-            let disableClass = !isFormFilled ? s.disabled : "";
-            if (unlockAfter === "none") disableClass = "";
-
-            return (
-              <li key={id}>
-                <Link href={link} className={`${activeClass} ${disableClass}`}>
-                  <SvgIcon name={iconName} />
-                  <span className={s.title}>{title}</span>
-                </Link>
-              </li>
-            );
-          }
-        )}
+        {BUILDER_SIDEBAR_PAGES.map((linkData) => (
+          <FormLink
+            key={linkData.id}
+            linkData={linkData}
+            shouldHideTitle={!isAsideOpen}
+          />
+        ))}
       </ul>
     </nav>
   );
