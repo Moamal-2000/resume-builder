c56e11a60c8f1e3240b8af89feb2e921139324e2
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-23 00:01:58 +0300

    feat(BuilderForm): add submission logic, validation, and make component reusable via props

--- src/components/Shared/Forms/BuilderForm/BuilderForm.jsx
+++ src/components/Shared/Forms/BuilderForm/BuilderForm.jsx
@@ -1,9 +1,41 @@
-import s from './BuilderForm.module.scss'
+"use client";
 
-const BuilderForm = () => {
+import useFormsStore from "@/stores/forms.store/forms.store";
+import { useRouter } from "next/navigation";
+import Button from "../../Buttons/Button/Button";
+import BuilderInputs from "../../Inputs/BuilderInputs/BuilderInputs";
+import s from "./BuilderForm.module.scss";
+
+const BuilderForm = ({
+  inputGroupKey,
+  nextPageRoute,
+  submitButtonText = "Submit",
+}) => {
+  const inputs = useFormsStore((s) => s[inputGroupKey]);
+  const router = useRouter();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const isValidForm = inputs.every(
+      (input) => input.hasValidValue || !input.required
+    );
+    if (!isValidForm) return;
+
+    router.push(nextPageRoute);
+  }
+
   return (
-    <form className={s.builderForm}></form>
-  )
-}
+    <form className={s.certificationForm} onSubmit={handleSubmit}>
+      <div className={s.inputs}>
+        <BuilderInputs inputGroupKey={inputGroupKey} />
+      </div>
 
-export default BuilderForm
+      <Button fillWidth={true} type="submit">
+        {submitButtonText}
+      </Button>
+    </form>
+  );
+};
+
+export default BuilderForm;
