f6af741d9046b7d027ae75dd60049d1dd1974ca2
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-24 02:38:25 +0300

    style(AddCertificationField): add basic styles & add 'plus' SVG icon

--- src/components/BuilderPage/SubPages/CertificationPage/AddCertificationField/AddCertificationField.jsx
+++ src/components/BuilderPage/SubPages/CertificationPage/AddCertificationField/AddCertificationField.jsx
@@ -1,9 +1,15 @@
-import s from './AddCertificationField.module.scss'
+import SvgIcon from "@/components/Shared/SvgIcon";
+import s from "./AddCertificationField.module.scss";
 
 const AddCertificationField = () => {
   return (
-    <button type='button'>AddCertificationField</button>
-  )
-}
+    <button type="button" className={s.button}>
+      <div className={s.icon}>
+        <SvgIcon name="plus" />
+      </div>
+      <span>Add Certification / License</span>
+    </button>
+  );
+};
 
-export default AddCertificationField
+export default AddCertificationField;
