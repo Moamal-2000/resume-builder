18b4e004f2e3d3b6390751df13daca158f8788a0
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-24 02:18:50 +0300

    init: AddCertificationField component

---
+++ src/components/BuilderPage/SubPages/CertificationPage/AddCertificationField/AddCertificationField.jsx
@@ -0,0 +1,9 @@
+import s from './AddCertificationField.module.scss'
+
+const AddCertificationField = () => {
+  return (
+    <button type='button'>AddCertificationField</button>
+  )
+}
+
+export default AddCertificationField
