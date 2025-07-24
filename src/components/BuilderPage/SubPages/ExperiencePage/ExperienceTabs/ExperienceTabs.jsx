bfbaecd004e4bf68ec8b81630e258433aa382077
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-24 04:36:25 +0300

    feat(ExperienceTabs): render all experience tabs

--- src/components/BuilderPage/SubPages/ExperiencePage/ExperienceTabs/ExperienceTabs.jsx
+++ src/components/BuilderPage/SubPages/ExperiencePage/ExperienceTabs/ExperienceTabs.jsx
@@ -5,16 +5,25 @@ import { useFormsStore } from "@/stores/forms.store/forms.store";
 import s from "./ExperienceTabs.module.scss";
 
 const ExperienceTabs = () => {
-  const experiencesInputs = useFormsStore((s) => s.experiencesInputs[0]);
-  const addExperience = useFormsStore((s) => s.addExperience);
-  const mainTabTitle = getInputValueOrFallback(experiencesInputs[0]);
+  const { addExperience, experiencesInputs } = useFormsStore((s) => s);
+  const mainExperienceInputs = experiencesInputs[0];
+  const mainTabTitle = getInputValueOrFallback(mainExperienceInputs[0]);
 
   return (
     <div className={s.experienceTabs}>
       <button type="button" className={`${s.mainTab} ${s.active}`}>
         {mainTabTitle}
       </button>
-      <button type="button" onClick={addExperience}>+ Add Experience</button>
+
+      {experiencesInputs.slice(1).map((experienceInput, index) => (
+        <button type="button" key={index}>
+          {getInputValueOrFallback(experienceInput[0])}
+        </button>
+      ))}
+
+      <button type="button" onClick={addExperience}>
+        + Add Experience
+      </button>
     </div>
   );
 };
