36c41a4e30d3ea220878e148ecac859a54f150fd
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-25 05:50:50 +0300

    feat(AddFieldButton): define jsx structure & add styles

--- src/components/Shared/Buttons/AddFieldButton/AddFieldButton.jsx
+++ src/components/Shared/Buttons/AddFieldButton/AddFieldButton.jsx
@@ -1,7 +1,15 @@
+import SvgIcon from "../../SvgIcon";
 import s from "./AddFieldButton.module.scss";
 
-const AddFieldButton = () => {
-  return <button type="button" className={s.button}></button>;
+const AddFieldButton = ({ label, onClick }) => {
+  return (
+    <button type="button" className={s.button} onClick={onClick}>
+      <div className={s.icon}>
+        <SvgIcon name="plus" />
+      </div>
+      <span>{label}</span>
+    </button>
+  );
 };
 
 export default AddFieldButton;
