d12476fd3ab6f54c606dc31c49d5985ac31e347d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-28 05:55:32 +0300

    style(ResumeDownloadSuccess): add basic styles with download button

--- src/components/DownloadPage/ResumeDownloadSuccess/ResumeDownloadSuccess.jsx
+++ src/components/DownloadPage/ResumeDownloadSuccess/ResumeDownloadSuccess.jsx
@@ -1,7 +1,22 @@
+import Button from "@/components/Shared/Buttons/Button/Button";
+import SvgIcon from "@/components/Shared/SvgIcon";
 import s from "./ResumeDownloadSuccess.module.scss";
 
 const ResumeDownloadSuccess = () => {
-  return <section className={s.downloadSection}></section>;
+  return (
+    <section className={s.downloadSection}>
+      <h1>Congratulation!</h1>
+
+      <p>
+        Your resume is awesome ready for download. Get it in any format of your
+        choice.
+      </p>
+
+      <Button paddingInline="90px">
+        Download <SvgIcon name="download" />
+      </Button>
+    </section>
+  );
 };
 
 export default ResumeDownloadSuccess;
