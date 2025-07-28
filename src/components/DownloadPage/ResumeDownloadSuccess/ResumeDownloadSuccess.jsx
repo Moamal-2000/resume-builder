8e13f6dcae300262d4428db8c4b7edfcb331d39a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-28 05:25:34 +0300

    init: ResumeDownloadSuccess component

---
+++ src/components/DownloadPage/ResumeDownloadSuccess/ResumeDownloadSuccess.jsx
@@ -0,0 +1,7 @@
+import s from "./ResumeDownloadSuccess.module.scss";
+
+const ResumeDownloadSuccess = () => {
+  return <section className={s.resumeDownloadSuccess}></section>;
+};
+
+export default ResumeDownloadSuccess;
