a7ba61ee913440fa4d80a60e0d3326a95c2f7416
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-28 05:33:44 +0300

    feat(DownloadPage): add UI structure

--- src/app/(pages)/download/page.js
+++ src/app/(pages)/download/page.js
@@ -1,9 +1,52 @@
+import LiveResume from "@/components/BuilderPage/LiveResume/LiveResume";
+import s from "@/components/DownloadPage/DownloadPage.module.scss";
+import ResumeDownloadSuccess from "@/components/DownloadPage/ResumeDownloadSuccess/ResumeDownloadSuccess";
+import SvgIcon from "@/components/Shared/SvgIcon";
+import Link from "next/link";
+
 export const metadata = {
   title: "Resume Builder | Download",
 };
 
 const DownloadPage = () => {
-  return <main>Download Page not yet implemented</main>;
+  return (
+    <main className={s.downloadPage}>
+      <div className="container">
+        <Link
+          href="/builder"
+          className={s.backButton}
+          title="Back to edit your resume"
+        >
+          <SvgIcon name="arrowLeft" />
+        </Link>
+
+        <div className={s.wrapper}>
+          <ResumeDownloadSuccess />
+
+          <section className={s.resumeSection}>
+            <div className={s.buttons}>
+              <Link
+                href="/preview"
+                className={s.previewButton}
+                title="Preview your resume"
+              >
+                Preview
+              </Link>
+              <Link
+                href="/builder"
+                className={s.editButton}
+                title="Edit your resume"
+              >
+                Edit
+              </Link>
+            </div>
+
+            <LiveResume />
+          </section>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default DownloadPage;
