aa918ddef126160af4524a30c0c5731b7850ebd5
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-30 05:24:17 +0300

    style(Certification): move styles from Certifications.module.scss to Certification.module.scss

--- src/components/Shared/Certification/Certification.jsx
+++ src/components/Shared/Certification/Certification.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import s from "./Certification.module.scss";
 
 const Certification = ({ data: [name, url] }) => {
   if (name === "") return null;
 
   return (
-    <li>
+    <li className={s.certificate}>
       <a href={url} target="_blank" rel="noreferrer noopenner">
         <Image
           width={26}
