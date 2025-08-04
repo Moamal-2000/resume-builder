0574d9588df0f6e31e13968f354a97be2c9abf70
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-04 14:13:00 +0300

    update(debugging): disable STOP_BUILDER_PROTECTION

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,4 +5,4 @@ export const SIDEBAR_ICON_DEBOUNCE_DELAY = 900;
 export const DESCRIPTION_PLACEHOLDER = `E.g. "Developed and maintained client-facing web applications using modern JavaScript frameworks. Collaborated with designers and back-end teams to deliver features on time."`;
 
 // Debugging
-export const STOP_BUILDER_PROTECTION = true;
+export const STOP_BUILDER_PROTECTION = false;
