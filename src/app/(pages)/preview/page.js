659b293f7c2ac4e55c7c8ec1d2ccfb977547370a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-05 15:43:59 +0300

    remove(preview page): remove /review route page with anything related to it

--- src/app/(pages)/preview/page.js
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: "Resume Builder | Preview",
-};
-
-const PreviewPage = () => {
-  return <main>Preview Page not yet implemented</main>;
-};
-
-export default PreviewPage;
--- src/components/DownloadPage/ResumeSection/ResumeSection.jsx
+++ src/components/DownloadPage/ResumeSection/ResumeSection.jsx
@@ -1,23 +1,9 @@
 import LiveResume from "@/components/BuilderPage/LiveResume/LiveResume";
-import Link from "next/link";
 import s from "./ResumeSection.module.scss";
 
 const ResumeSection = () => {
   return (
     <section className={s.resumeSection}>
-      <div className={s.buttons}>
-        <Link
-          href="/preview"
-          className={s.previewButton}
-          title="Preview your resume"
-        >
-          Preview
-        </Link>
-        <Link href="/builder" className={s.editButton} title="Edit your resume">
-          Edit
-        </Link>
-      </div>
-
       <LiveResume />
     </section>
   );
