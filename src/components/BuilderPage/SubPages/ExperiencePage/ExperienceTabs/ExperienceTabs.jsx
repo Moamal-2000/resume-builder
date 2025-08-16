297ddb671f8686c07ab86810ce6fb6f66a817370
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-16 03:30:40 +0300

    refactor(ExperienceTabs): simplify component by isolate main tab into MainExperienceTab component

--- src/components/BuilderPage/SubPages/ExperiencePage/ExperienceTabs/ExperienceTabs.jsx
+++ src/components/BuilderPage/SubPages/ExperiencePage/ExperienceTabs/ExperienceTabs.jsx
@@ -5,6 +5,7 @@ import { EXPERIENCE_MAX_COUNT } from "@/data/constants";
 import { getInputValueOrFallback, hasFormFilled } from "@/functions/helper";
 import { useFormsStore } from "@/stores/forms.store/forms.store";
 import s from "./ExperienceTabs.module.scss";
+import MainExperienceTab from "./MainExperienceTab/MainExperienceTab";
 
 const ExperienceTabs = () => {
   const {
@@ -45,27 +46,7 @@ const ExperienceTabs = () => {
 
   return (
     <div className={s.experienceTabs}>
-      <button
-        type="button"
-        className={`${s.mainTab} ${experiencesTabIndex === 0 ? s.active : ""} ${
-          formsFilled[0] ? "" : s.invalidForm
-        }`}
-        onClick={() => updateTabIndex(0)}
-      >
-        {mainTabTitle}
-
-        {formsFilled[0] && (
-          <span className={`${s.iconHolder} ${s.checkMark}`}>
-            <SvgIcon name="checked" />
-          </span>
-        )}
-
-        {!formsFilled[0] && (
-          <span className={`${s.iconHolder} ${s.warning}`}>
-            <SvgIcon name="warning" />
-          </span>
-        )}
-      </button>
+    <MainExperienceTab formsFilled={formsFilled} title={mainTabTitle} />
 
       {experiencesInputs.slice(1).map((experienceInput, index) => {
         console.log(formsFilled[index + 1]);
--- src/components/BuilderPage/SubPages/ExperiencePage/ExperienceTabs/MainExperienceTab/MainExperienceTab.jsx
+++ src/components/BuilderPage/SubPages/ExperiencePage/ExperienceTabs/MainExperienceTab/MainExperienceTab.jsx
@@ -1,9 +1,35 @@
-import s from './MainExperienceTab.module.scss'
+"use client";
 
-const MainExperienceTab = () => {
+import SvgIcon from "@/components/Shared/SvgIcon";
+import { useFormsStore } from "@/stores/forms.store/forms.store";
+import s from "./MainExperienceTab.module.scss";
+
+const MainExperienceTab = ({ formsFilled, title }) => {
+  const { updateTabIndex, experiencesTabIndex } = useFormsStore((s) => s);
+
   return (
-    <div>MainExperienceTab</div>
-  )
-}
+    <button
+      type="button"
+      className={`${s.mainTab} ${experiencesTabIndex === 0 ? s.active : ""} ${
+        formsFilled[0] ? "" : s.invalidForm
+      }`}
+      onClick={() => updateTabIndex(0)}
+    >
+      {title}
 
-export default MainExperienceTab
+      {formsFilled[0] && (
+        <span className={`${s.iconHolder} ${s.checkMark}`}>
+          <SvgIcon name="checked" />
+        </span>
+      )}
+
+      {!formsFilled[0] && (
+        <span className={`${s.iconHolder} ${s.warning}`}>
+          <SvgIcon name="warning" />
+        </span>
+      )}
+    </button>
+  );
+};
+
+export default MainExperienceTab;
