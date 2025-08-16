cd21307965abf3de0b011cb8fb9aab0ae69b6537
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-16 18:46:33 +0300

    feat(DynamicIcon): add tooltip to indicate form status

--- src/components/Shared/DynamicIcon/DynamicIcon.jsx
+++ src/components/Shared/DynamicIcon/DynamicIcon.jsx
@@ -2,17 +2,30 @@ import SvgIcon from "../SvgIcon";
 import s from "./DynamicIcon.module.scss";
 
 const DynamicIcon = ({ hasFormFilled, isFormEmpty }) => {
-  if (hasFormFilled)
-    return (
-      <span className={`${s.iconHolder} ${s.checkMark}`}>
-        <SvgIcon name="checked" />
-      </span>
-    );
+  const formStatus = hasFormFilled
+    ? "completely filled"
+    : isFormEmpty
+    ? "Empty"
+    : "partially filled";
 
   return (
-    <span className={`${s.iconHolder} ${isFormEmpty ? s.warning : s.error}`}>
-      <SvgIcon name="warning" />
-    </span>
+    <div className={s.mainWrapper}>
+      {hasFormFilled && (
+        <span className={`${s.iconHolder} ${s.checkMark}`}>
+          <SvgIcon name="checked" />
+        </span>
+      )}
+
+      {!hasFormFilled && (
+        <span
+          className={`${s.iconHolder} ${isFormEmpty ? s.warning : s.error}`}
+        >
+          <SvgIcon name="warning" />
+        </span>
+      )}
+
+      <p className={s.tooltip}>The form is {formStatus}</p>
+    </div>
   );
 };
 
