493580791ad676ce2f7db9900e4930cf0ac43741
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-24 01:24:32 +0300

    feat(LiveResume3): add Experiences component with styles aligned to resume design

--- src/components/BuilderPage/LiveResume3/Experiences/JobExperience/JobExperience.jsx
+++ src/components/BuilderPage/LiveResume3/Experiences/JobExperience/JobExperience.jsx
@@ -3,12 +3,15 @@ import s from "./JobExperience.module.scss";
 const JobExperience = ({ title, company, startDate, endDate, description }) => {
   return (
     <div className={s.jobExperience}>
-      <h3 className={s.title}>
-        {title} | {company}
-      </h3>
-      <time className={s.date}>
-        {startDate} - {endDate}
-      </time>
+      <div className={s.wrapper}>
+        <h3 className={s.title}>
+          {title} | {company}
+        </h3>
+        <time className={s.date}>
+          {startDate} - {endDate}
+        </time>
+      </div>
+
       <p className={s.description}>{description}</p>
     </div>
   );
--- src/components/BuilderPage/LiveResume3/LiveResume3.jsx
+++ src/components/BuilderPage/LiveResume3/LiveResume3.jsx
@@ -1,3 +1,4 @@
+import Experiences from "./Experiences/Experiences";
 import s from "./LiveResume3.module.scss";
 import ProfileHeader from "./ProfileHeader/ProfileHeader";
 
@@ -5,7 +6,7 @@ const LiveResume3 = () => {
   return (
     <section className={s.liveResume}>
       <ProfileHeader />
-      {/* <Experiences /> */}
+      <Experiences />
       {/* <TechSkills /> */}
       {/* <Education /> */}
       {/* <Certifications /> */}
