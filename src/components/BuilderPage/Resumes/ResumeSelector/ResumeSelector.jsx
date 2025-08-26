61fa4934636fbc286fe42ce3d26ea59c445df533
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-26 08:12:35 +0300

    feat(ResumeSelector): create custom radio selector

--- src/components/BuilderPage/Resumes/ResumeSelector/ResumeSelector.jsx
+++ src/components/BuilderPage/Resumes/ResumeSelector/ResumeSelector.jsx
@@ -1,7 +1,23 @@
+import { RESUMES } from "@/data/staticData";
+import Image from "next/image";
 import s from "./ResumeSelector.module.scss";
 
 const ResumeSelector = () => {
-  return <div className={s.selector}></div>;
+  return (
+    <div className={s.selector}>
+      {RESUMES.map(({ name, imagePath, id }) => (
+        <div className={s.resumeFrame} key={id}>
+          <Image
+            src={imagePath}
+            alt="Resume template"
+            width={134}
+            height={160}
+          />
+          <input type="radio" name="resume" value={name} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ResumeSelector;
