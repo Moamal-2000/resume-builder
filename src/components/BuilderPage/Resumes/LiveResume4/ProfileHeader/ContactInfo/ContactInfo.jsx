cf5fa25c87e3760461e58ec271ed7d895c023c32
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-01 09:32:40 +0300

    refactor(ProfileHeader): simplify component by isolate contact info UI into ContactInfo component

--- src/components/BuilderPage/Resumes/LiveResume4/ProfileHeader/ContactInfo/ContactInfo.jsx
+++ src/components/BuilderPage/Resumes/LiveResume4/ProfileHeader/ContactInfo/ContactInfo.jsx
@@ -1,9 +1,34 @@
-import s from './ContactInfo.module.scss'
+"use client";
 
-const ContactInfo = () => {
+import { getContactInfo } from "@/functions/helper";
+import { useFormsStore } from "@/stores/forms.store/forms.store";
+import s from "./ContactInfo.module.scss";
+
+const ContactInfo = ({ email, address }) => {
+  const contactInfoInputs = useFormsStore((s) => s.contactInfoInputs);
+
+  const [emailData, phoneData, linkedinData, githubData, personalWebsiteData] =
+    getContactInfo(contactInfoInputs, email);
+
   return (
-    <div>ContactInfo</div>
-  )
-}
+    <>
+      <p className={s.info}>
+        {address}
+        <span className={s.verticalBar} />
+        {emailData.title}
+        <span className={s.verticalBar} />
+        {personalWebsiteData.title}
+      </p>
 
-export default ContactInfo
+      <p className={s.info}>
+        {phoneData.title}
+        <span className={s.verticalBar} />
+        {linkedinData.title}
+        <span className={s.verticalBar} />
+        {githubData.title}
+      </p>
+    </>
+  );
+};
+
+export default ContactInfo;
--- src/components/BuilderPage/Resumes/LiveResume4/ProfileHeader/ProfileHeader.jsx
+++ src/components/BuilderPage/Resumes/LiveResume4/ProfileHeader/ProfileHeader.jsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { getContactInfo, getInputValuesByName } from "@/functions/helper";
+import { getInputValuesByName } from "@/functions/helper";
 import { useFormsStore } from "@/stores/forms.store/forms.store";
 import SectionTitle from "../SectionTitle/SectionTitle";
+import ContactInfo from "./ContactInfo/ContactInfo";
 import s from "./ProfileHeader.module.scss";
 
 const ProfileHeader = () => {
-  const { personalInfoInputs, contactInfoInputs } = useFormsStore((s) => s);
+  const personalInfoInputs = useFormsStore((s) => s.personalInfoInputs);
 
   const { fullName, profession, email, summery, address } =
     getInputValuesByName(personalInfoInputs);
-
-  const [emailData, phoneData, linkedinData, githubData, personalWebsiteData] =
-    getContactInfo(contactInfoInputs, email);
 
   return (
     <section className={s.profileHeader}>
@@ -20,20 +18,7 @@ const ProfileHeader = () => {
         <h2 className={s.name}>{fullName}</h2>
         <h2 className={s.profession}>{profession}</h2>
 
-        <p className={s.info}>
-          {address}
-          <span className={s.verticalBar} />
-          {emailData.title}
-          <span className={s.verticalBar} />
-          {personalWebsiteData.title}
-        </p>
-        <p className={s.info}>
-          {phoneData.title}
-          <span className={s.verticalBar} />
-          {linkedinData.title}
-          <span className={s.verticalBar} />
-          {githubData.title}
-        </p>
+        <ContactInfo email={email} address={address} />
       </header>
 
       <footer>
