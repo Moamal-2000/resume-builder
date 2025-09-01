c0988c5e98a5d3956074ad2beb74ab111940796c
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-01 09:06:55 +0300

    feat(LiveResume4): add TechSkills component with styles aligned to resume design

--- src/components/BuilderPage/Resumes/LiveResume4/LiveResume4.jsx
+++ src/components/BuilderPage/Resumes/LiveResume4/LiveResume4.jsx
@@ -1,13 +1,14 @@
 import Experiences from "./Experiences/Experiences";
 import s from "./LiveResume4.module.scss";
 import ProfileHeader from "./ProfileHeader/ProfileHeader";
+import TechSkills from "./TechSkills/TechSkills";
 
 const LiveResume4 = () => {
   return (
     <section className={s.liveResume}>
       <ProfileHeader />
       <Experiences />
-      {/* <TechSkills /> */}
+      <TechSkills />
       {/* <Education /> */}
       {/* <Certifications /> */}
     </section>
