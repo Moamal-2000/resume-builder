29698d4e7a4fe7a446d206c78315a50cbb183183
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-01 10:12:17 +0300

    fix(ResumeWrapper): allow React to read hooks before handling early return

--- src/components/BuilderPage/BuilderRootLayout/ResumeWrapper/ResumeWrapper.jsx
+++ src/components/BuilderPage/BuilderRootLayout/ResumeWrapper/ResumeWrapper.jsx
@@ -5,12 +5,12 @@ import useGlobalStore from "@/stores/global.store/global.store";
 import { usePathname } from "next/navigation";
 
 const ResumeWrapper = () => {
+  const selectedResume = useGlobalStore((s) => s.selectedResume);
   const pathname = usePathname();
   const isRootPage = pathname === "/builder";
 
-  if (isRootPage) return;
+  if (isRootPage) return null;
 
-  const selectedResume = useGlobalStore((s) => s.selectedResume);
   const selectedResumeData = RESUMES.find(
     (resume) => resume.name === selectedResume
   );
