c338c140753c34b5ffda6d48716edb3ac6baf3db
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-03 15:41:56 +0300

    feat(ResumesPage): display "More resumes coming soon" message below available resumes

--- src/app/(pages)/builder/page.js
+++ src/app/(pages)/builder/page.js
@@ -1,7 +1,16 @@
+import s from "@/components/BuilderPage/Resumes/ResumePage.module.scss";
 import ResumeSelector from "@/components/BuilderPage/Resumes/ResumeSelector/ResumeSelector";
 
 const ResumesPage = () => {
-  return <ResumeSelector />;
+  return (
+    <div className={s.resumePage}>
+      <ResumeSelector />
+
+      <p className={s.comingSoonMessage}>
+        More resumes will be available soon!
+      </p>
+    </div>
+  );
 };
 
 export default ResumesPage;
