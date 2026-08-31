ca703ce802a8a4e892b8273887d270d234fc7932
Moamal-2000 <moamalalaapro1@gmail.com>
2026-08-31 10:54:43 +0300

    chore(next.config): force redirect from resume-builder-moamal.vercel.app to resume-builder.moamalalaa.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 const nextConfig = {
   devIndicators: false,
   reactCompiler: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          { type: "host", value: "https://resume-builder-moamal.vercel.app" },
+        ],
+        destination: "https://resume-builder.moamalalaa.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
